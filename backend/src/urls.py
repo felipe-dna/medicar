1789172ef6977dcc23a6853414b06f5b014a166c
Felipe Amador <felipedavidamador@gmail.com>
2020-10-12 17:13:29 -0300

    feat: add docs to project urls file

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -1,13 +1,17 @@
+"""
+Contains the Medicar API urls settings.
+TODO: add path to docs.
+"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# Static and media URLS settings.
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
