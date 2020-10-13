93ac1e937baa0a3d215f6dfc29c640988332620a
Felipe Amador <felipedavidamador@gmail.com>
2020-10-13 00:50:13 -0300

    feat: add the Doctor view set

---
+++ backend/apps/doctors/viewsets.py
@@ -0,0 +1,18 @@
+"""
+Contains the doctors app view sets.
+
+https://www.django-rest-framework.org/api-guide/viewsets/
+"""
+
+from rest_framework import viewsets
+
+from apps.doctors.models import Doctor
+from apps.doctors.serializers import DoctorSerializer
+
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    """Contains the Doctor view set."""
+
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = []
