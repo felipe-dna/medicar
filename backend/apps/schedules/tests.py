1469910046a67675d8b2b2c85b9376e3f7634f0e
Felipe Amador <felipedavidamador@gmail.com>
2020-10-12 21:57:41 -0300

    feat: add a test to the DoctorScheduleTime model

--- backend/apps/schedules/tests.py
+++ backend/apps/schedules/tests.py
@@ -1,3 +1,24 @@
+"""
+Contains the schedules app tests.
+
+https://docs.djangoproject.com/en/3.1/topics/testing/overview/
+"""
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from .models import DoctorScheduleTime, DoctorSchedule
+
+
+class DoctorScheduleTimeModelTestCase(TestCase):
+    """Define the doctor schedule time model tests."""
+
+    def test_create_a_schedule_time_passing_wrong_time_format(self) -> None:
+        """"""
+        invalid_time_format = 'a invalid time format'
+        expected_message = f"{invalid_time_format}” value has an invalid " \
+                           f"format. It must be in HH:MM[:ss[.uuuuuu]] format."
+
+        with self.assertRaisesMessage(ValidationError, str(expected_message)):
+            DoctorScheduleTime.objects.create(time=invalid_time_format)
+
