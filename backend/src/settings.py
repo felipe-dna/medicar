98fb80f5f0244f163ca8c6c569741f2239132373
Felipe Amador <felipedavidamador@gmail.com>
2020-10-12 21:20:09 -0300

    fix: break a line between the default apps and the 3rd party apps

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # 3rd party apps.
     'admin_interface',
     'colorfield',
