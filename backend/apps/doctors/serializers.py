d9bce8f248357af51a6e314819be89352c02f3fb
Felipe Amador <felipedavidamador@gmail.com>
2020-10-13 12:36:31 -0300

    feat: set the nested fields for the speciality model

--- backend/apps/doctors/serializers.py
+++ backend/apps/doctors/serializers.py
@@ -4,17 +4,29 @@ Contains the doctors app serializers.
 https://www.django-rest-framework.org/api-guide/serializers/
 """
 
+from drf_writable_nested import WritableNestedModelSerializer
 from rest_framework import serializers
 
 from apps.doctors.models import Doctor
+from apps.specialties.models import Speciality
 
 
-class DoctorSerializer(serializers.ModelSerializer):
+class SpecialitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Speciality
+        fields = ['id', 'name']
+        read_only_fields = ['name']
+
+
+class DoctorSerializer(WritableNestedModelSerializer):
     """
     Contains the Doctor Serializer class.
     """
+    speciality = SpecialitySerializer(many=False)
 
     class Meta:
         model = Doctor
-        fields = '__all__'
-        read_only_fields = ['id']
+        fields = [
+            'id', 'name', 'crm', 'email', 'phone', 'speciality', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at', 'speciality']
