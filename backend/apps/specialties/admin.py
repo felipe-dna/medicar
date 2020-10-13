bcefdd69a4bcf1173b86d43019118f354017e6e2
Felipe Amador <felipedavidamador@gmail.com>
2020-10-13 12:33:58 -0300

    feat: define the SpecialityAdmin object

--- backend/apps/specialties/admin.py
+++ backend/apps/specialties/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 
 from .models import Speciality
 
-admin.site.register(Speciality)
+
+class SpecialityAdmin(admin.ModelAdmin):
+    """Define the admin functionalities for the Speciality app."""
+    fields = ('id', 'name', 'created_at', 'updated_at')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+
+admin.site.register(Speciality, SpecialityAdmin)
+
