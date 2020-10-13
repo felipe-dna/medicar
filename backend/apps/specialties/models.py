3130e748f7557a17bad718dfd1853efe15c3be2c
Felipe Amador <felipedavidamador@gmail.com>
2020-10-12 21:31:26 -0300

    feat: add the meta class and override the save method

--- backend/apps/specialties/models.py
+++ backend/apps/specialties/models.py
@@ -8,6 +8,13 @@ class Speciality(UUIDTimeControlMethod):
     """Define the Speciality model."""
     name = models.CharField(max_length=100, unique=True)
 
+    class Meta:
+        db_table = 'speciality'
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(Speciality, self).save(*args, **kwargs)
+
     def __str__(self) -> str:
         """Return a string representation for Speciality model instance."""
         return self.name
