a05db94b441954384804e867e06e6e71ee76e8ab
Felipe Amador <felipedavidamador@gmail.com>
2020-10-13 00:04:42 -0300

    fix: add docstrings and format the base model

--- backend/apps/base/models.py
+++ backend/apps/base/models.py
@@ -1,13 +1,19 @@
-""""""
+"""Contains the base model used in the other models."""
 import uuid
 
 from django.db import models
 
 
 class UUIDTimeControlMethod(models.Model):
-    """"""
+    """
+    Define a abstract model that provides a uuid, created_at and updated_at
+    fields.
+    """
     id = models.UUIDField(
-        primary_key=True, editable=False, unique=True, default=uuid.uuid4
+        primary_key=True,
+        editable=False,
+        unique=True,
+        default=uuid.uuid4
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
