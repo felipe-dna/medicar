f863e09b4fd02e22effb72ac41144c70316629f5
Felipe Amador <felipedavidamador@gmail.com>
2020-10-12 22:45:52 -0300

    feat: add docstrings to the Doctor model

--- backend/apps/doctors/models.py
+++ backend/apps/doctors/models.py
@@ -1,22 +1,51 @@
-""""""
-import uuid
-
+"""Contains the Medicar API doctors app models."""
+from django.core.validators import EmailValidator
 from django.db import models
+
+from django.utils.translation import ugettext_lazy as _
 
 from apps.base.models import UUIDTimeControlMethod
 from apps.specialties.models import Speciality
 
 
 class Doctor(UUIDTimeControlMethod):
-    """"""
-    id = models.UUIDField(
-        primary_key=True, editable=False, unique=True, default=uuid.uuid4
+    """Define the Doctor model."""
+    name = models.CharField(
+        verbose_name=_('name'),
+        max_length=60
     )
-    name = models.CharField(max_length=100)
-    crm = models.CharField(max_length=160, unique=True)
-    email = models.EmailField(max_length=200, unique=True)
-    phone = models.CharField(max_length=11)
-    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+    crm = models.PositiveIntegerField(
+        max_length=4,
+        unique=True
+    )
+    email = models.EmailField(
+        max_length=150,
+        unique=True,
+        validators=[
+            EmailValidator
+        ]
+    )
+    phone = models.CharField(
+        verbose_name=_('phone'),
+        max_length=11
+    )
+    speciality = models.ForeignKey(
+        to=Speciality,
+        verbose_name=_('speciality'),
+        on_delete=models.CASCADE
+    )
+
+    def save(self, *args, **kwargs) -> None:
+        """
+        Override the models.Model.save() method to call the
+        models.Model.full_clean() method to validate all the fields before
+        save.
+        """
+        self.full_clean()
+        super(Doctor, self).save(*args, **kwargs)
 
     def __str__(self) -> str:
+        """
+        Return a string representation for a User model instance.
+        """
         return f"Dr. {self.name}"
