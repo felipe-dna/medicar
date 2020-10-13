1b5df76017ebceedb308f864b044461ffdc46f40
Felipe Amador <felipedavidamador@gmail.com>
2020-10-12 21:39:32 -0300

    feat: add docstrings to many methods

--- backend/apps/schedules/models.py
+++ backend/apps/schedules/models.py
@@ -1,6 +1,4 @@
-""""""
-import uuid
-
+"""Contains the Medicar API users app models."""
 from django.db import models
 
 from apps.base.models import UUIDTimeControlMethod
@@ -9,30 +7,61 @@ from apps.schedules.validators import validate_schedule_date
 
 
 class DoctorScheduleTime(UUIDTimeControlMethod):
-    id = models.UUIDField(
-        primary_key=True, editable=False, unique=True, default=uuid.uuid4
-    )
+    """Define the ScheduleTime model."""
     time = models.TimeField()
 
+    class Meta:
+        db_table = 'doctor_schedule_time'
+
+    def save(self, *args, **kwargs) -> None:
+        """
+        Override the models.Model.save() method to call the
+        models.Model.full_clean() method to validate all the fields before
+        save.
+        """
+        self.full_clean()
+        super(DoctorScheduleTime, self).save(*args, **kwargs)
+
     def __str__(self) -> str:
+        """
+        Return a string representation for a DoctorScheduleTime model instance.
+        """
         return self.time.strftime('%HH:%MM:%SS')
 
 
 class DoctorSchedule(UUIDTimeControlMethod):
-    """"""
-    id = models.UUIDField(
-        primary_key=True, editable=False, unique=True, default=uuid.uuid4
+    """Define the DoctorSchedule model."""
+    doctor = models.ForeignKey(
+        to=Doctor,
+        on_delete=models.CASCADE
     )
-    doctor = models.ForeignKey(to=Doctor, on_delete=models.CASCADE)
-    day = models.DateField(validators=[validate_schedule_date])
-    available_times = models.ManyToManyField(to=DoctorScheduleTime, blank=True)
+    day = models.DateField(
+        validators=[
+            validate_schedule_date
+        ]
+    )
+    available_times = models.ManyToManyField(
+        to=DoctorScheduleTime,
+        blank=True
+    )
 
     class Meta:
-        """"""
         # https://docs.djangoproject.com/en/3.1/ref/models/options/#unique-together
         unique_together = [['doctor', 'day']]
+        db_table = 'doctor_schedule'
+
+    def save(self, *args, **kwargs) -> None:
+        """
+        Override the models.Model.save() method to call the
+        models.Model.full_clean() method to validate all the fields before
+        save.
+        """
+        self.full_clean()
+        super(DoctorSchedule, self).save(*args, **kwargs)
 
     def __str__(self) -> str:
-        """"""
+        """
+        Return a string representation for a DoctorSchedule model instance.
+        """
         return f'Dr. {self.doctor.name} - {self.day.strftime("%dd/mm/YY")}'
 
