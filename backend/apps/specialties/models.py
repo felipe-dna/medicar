d336bdd69a79c0a5f00258cfc471f3c70ab90aef
Felipe Amador <felipedavidamador@gmail.com>
2020-10-12 21:23:23 -0300

    fix: remove useless field

--- backend/apps/specialties/models.py
+++ backend/apps/specialties/models.py
@@ -1,6 +1,4 @@
 """Contains the Medicar API specialties app models."""
-import uuid
-
 from django.db import models
 
 from apps.base.models import UUIDTimeControlMethod
@@ -8,9 +6,6 @@ from apps.base.models import UUIDTimeControlMethod
 
 class Speciality(UUIDTimeControlMethod):
     """Define the Speciality model."""
-    id = models.UUIDField(
-        primary_key=True, editable=False, unique=True, default=uuid.uuid4
-    )
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self) -> str:
