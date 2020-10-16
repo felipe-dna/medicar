cd1b4503422f7e160dbea68544a1a6e6eaf99419
Felipe Amador <felipedavidamador@gmail.com>
2020-10-16 20:16:41 -0300

    feat: add docs for the schedules validators

--- backend/apps/schedules/validators.py
+++ backend/apps/schedules/validators.py
@@ -1,10 +1,16 @@
+"""Contains the schedules app validators."""
 from datetime import date
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 
-def validate_schedule_date(value: date) -> None:
-    """"""
-    if value < date.today():
+def validate_schedule_date(time_value: date) -> None:
+    """
+    Checks if the current time value it's a past date.
+
+    :param time_value: The current time value.
+    :type time_value: datetime.date.
+    """
+    if time_value < date.today():
         raise ValidationError(_("You cannot send a past date."))
