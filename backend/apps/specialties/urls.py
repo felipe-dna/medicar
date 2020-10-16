302617db0191920864325ea3f2fbebde72d89213
Felipe Amador <felipedavidamador@gmail.com>
2020-10-16 19:25:00 -0300

    feat: add the specialties routes

---
+++ backend/apps/specialties/urls.py
@@ -0,0 +1,19 @@
+"""
+Contains the specialties app urls.
+
+https://www.django-rest-framework.org/api-guide/routers/
+"""
+
+from django.urls import include, path
+from rest_framework import routers
+
+from .viewsets import SpecialityViewSet
+
+app_name = 'specialties'
+
+router = routers.SimpleRouter(trailing_slash=False)
+router.register('doctors', SpecialityViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
