a4b33325d8ed9494f64e35a2d5e0a080e647bd9d
Felipe Amador <felipedavidamador@gmail.com>
2020-10-16 19:45:02 -0300

    fix: remove the updated_at field

--- backend/apps/specialties/serializers.py
+++ backend/apps/specialties/serializers.py
@@ -13,8 +13,8 @@ class SpecialitySerializer(serializers.ModelSerializer):
     """Define the serializer class used to read and create specialties."""
     class Meta:
         model = Speciality
-        fields = ['id', 'name', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'created_at']
+        read_only_fields = ['id', 'created_at']
 
 
 class SpecialityForeignKeySerializer(serializers.ModelSerializer):
