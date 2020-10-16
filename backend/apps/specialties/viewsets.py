4507d97d47d5bc628453997152042aa774e8a031
Felipe Amador <felipedavidamador@gmail.com>
2020-10-16 19:34:54 -0300

    feat: create the specialties app view set

--- backend/apps/specialties/viewsets.py
+++ backend/apps/specialties/viewsets.py
@@ -4,9 +4,21 @@ Contains the specialties app view sets.
 https://www.django-rest-framework.org/api-guide/viewsets/
 """
 
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
+
+from .models import Speciality
+from .serializers import SpecialitySerializer
 
 
 class SpecialityViewSet(viewsets.ViewSet):
-    """"""
+    """Contains the Speciality view set."""
 
+    http_method_names = ('options', 'post', 'get')
+    permission_classes = []
+    queryset = Speciality.objects.all()
+    serializer_class = SpecialitySerializer
+
+    # Filter settings.
+    # https://www.django-rest-framework.org/api-guide/filtering/#searchfilter
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
