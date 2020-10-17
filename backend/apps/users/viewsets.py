a77491a75a8e9a3e21df020c7bd9f31c74fdd126
Felipe Amador <felipedavidamador@gmail.com>
2020-10-17 14:21:31 -0300

    feat: create the LoginViewSet

--- backend/apps/users/viewsets.py
+++ backend/apps/users/viewsets.py
@@ -4,16 +4,40 @@ Contains the users app view sets.
 https://www.django-rest-framework.org/api-guide/viewsets/
 """
 
-from rest_framework import viewsets
-from rest_framework.request import Request
-from rest_framework.response import Response
+from django.utils.translation import gettext_lazy as _
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.compat import coreapi, coreschema
+from rest_framework.schemas import ManualSchema
+from rest_framework.schemas import coreapi as coreapi_schema
 
-from apps.users.models import User
-from apps.users.serializers import UserSerializer
+from apps.users.serializers import LoginSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """"""
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = []
+class LoginViewSet(ObtainAuthToken):
+    """Define the login view set."""
+    serializer_class = LoginSerializer
+
+    if coreapi_schema.is_enabled():
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name="email",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Email",
+                        description=_("Valid email for authentication"),
+                    ),
+                ),
+                coreapi.Field(
+                    name="password",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title=_("Password"),
+                        description=_("Valid password for authentication"),
+                    ),
+                ),
+            ],
+            encoding="application/json",
+        )
