fcae50b6a94059295ef2e754528b90e67b0d050e
Felipe Amador <felipedavidamador@gmail.com>
2020-10-16 23:39:51 -0300

    feat: add a signal to generate a Token to each created user

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -6,9 +6,11 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.core.validators import EmailValidator
 from django.db import models
+from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 
 from apps.users.managers import UserManager
+from apps.users.signals import generate_user_token
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -70,3 +72,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         Return a string representation for a User model instance.
         """
         return self.email
+
+
+post_save.connect(generate_user_token, sender=User)
---
+++ backend/apps/users/signals.py
@@ -0,0 +1,17 @@
+"""Contain the users apps signals."""
+from rest_framework.authtoken.models import Token
+
+
+def generate_user_token(_, instance, created: bool, **kwargs) -> None:
+    """
+    Called when a new user is created, it generate a new Token for this user.
+
+    :param _: The model class that just had an instance created.
+    :type _: users.models.User.
+    :param instance: The created user instance.
+    :type instance: users.models.User.
+    :param created: A boolean value that defines if the user is being created or updated.
+    :type created: bool
+    """
+    if created:
+        Token.objects.create(user=instance)
