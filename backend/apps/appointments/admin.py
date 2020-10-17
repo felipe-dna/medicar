41702a21429a75f0e54fa17e2662150760b8b1f3
Felipe Amador <felipedavidamador@gmail.com>
2020-10-17 20:41:06 -0300

    feat: add the appointments admin configuration

--- backend/apps/appointments/admin.py
+++ backend/apps/appointments/admin.py
@@ -1,3 +1,13 @@
+"""Contains the admin settings for the appointments resource."""
 from django.contrib import admin
 
-# Register your models here.
+from .models import Appointment
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    """Define the admin interface for the appointments resource."""
+    fields = ('id', 'day', 'time', 'doctor', 'patient', 'created_at')
+    readonly_fields = ('id', 'created_at')
+
+
+admin.site.register(Appointment, AppointmentAdmin)
