419614c325fcaff99ef3f1e4f610559cd32bed53
Felipe Amador <felipedavidamador@gmail.com>
2020-10-25 16:19:06 -0300

    feat: add login and logout routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './shared/auth.guard';
+import { LogoutComponent } from './pages/logout/logout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [AuthGuard]
   },
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'logout',
+        component: LogoutComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
