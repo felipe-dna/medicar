9de6ab45001713208bde805e7f09664cd01607aa
Felipe Amador <felipedavidamador@gmail.com>
2020-10-25 09:22:32 -0300

    feat: add modal to add a new appointment

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,34 +4,40 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AppointmentFormComponent } from './pages/home/components/appointment-form/appointment-form.component';
 import { HttpClientModule } from '@angular/common/http';
 
 // Angular Material.
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AppointmentFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatFormFieldModule,
-    MatSliderModule,
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
     MatButtonModule,
     MatTableModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../shared/service/api.service';
-import { Appointment } from '../../shared/models/Appointment.model';
+import { Appointment, Speciality } from '../../shared/models/Appointment.model';
+import { MatDialog } from '@angular/material/dialog';
+import { AppointmentFormComponent } from './components/appointment-form/appointment-form.component';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +13,34 @@ import { Appointment } from '../../shared/models/Appointment.model';
 export class HomeComponent implements OnInit {
   appointmentList: Appointment[];
   displayedColumns: string[] = ['ESPECIALIDADE', 'PROFISSIONAL', 'DATA', 'HORA'];
+  speciality: string;
+  doctor: string;
+  date: string;
+  time: string;
 
   constructor(
-    public apiService: ApiService
+    public apiService: ApiService,
+    public appointmentDialog: MatDialog
   ) {}
+
+  openDialog(): void {
+    const dialogRef = this.appointmentDialog.open(AppointmentFormComponent, {
+      id: 'appointment-form-dialog',
+      width: '480px',
+      height: '420px',
+      data: {
+        speciality: this.speciality,
+        doctor: this.doctor,
+        date: this.date,
+        time: this.time
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.doctor = result;
+    });
+  }
 
   ngOnInit(): void {
     this.getAppointments();
@@ -22,9 +48,6 @@ export class HomeComponent implements OnInit {
 
   // tslint:disable-next-line:typedef
   getAppointments() {
-    this.apiService.getMedicalAppointments().subscribe(data => {
-      console.log(data);
-      this.appointmentList = data;
-    });
+    this.apiService.getMedicalAppointments().subscribe(data => this.appointmentList = data);
   }
 }
